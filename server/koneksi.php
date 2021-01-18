2f37d83084c55fde03e97c5d103491c53d6fac95
jevonas <67134221+jevonas@users.noreply.github.com>
2021-01-18 12:06:12 +0700

    Create koneksi.php

---
+++ server/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "backend";
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+if (!$connection){
+        die("Connection Failed:".mysqli_connect_error());
+    }
+    
+?>
